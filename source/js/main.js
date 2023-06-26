54d0dcd42f8951a1be149b00e4ced03b499305ae
Shchipanov <chips_80@mail.ru>
2023-06-26 17:21:47 +0400

    feat: начинает каталог

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,5 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 
-// import {initVideo} from './modules/init-video';
 import {addText} from './modules/adding-text';
 import {sliderTrainers, removeNotJs, sliderAdvt} from './modules/slider';
 // ---------------------------------
@@ -18,7 +17,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    // initVideo();
     addText();
     sliderTrainers();
     removeNotJs();
