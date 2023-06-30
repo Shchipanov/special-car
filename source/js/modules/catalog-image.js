c3b8c52469f435597d12191d325b0aaa23577dac
Shchipanov <chips_80@mail.ru>
2023-06-30 19:03:35 +0400

    fix: исправляет шапку

--- source/js/modules/catalog-image.js
+++ source/js/modules/catalog-image.js
@@ -1,13 +1,30 @@
-const wrappers = document.querySelectorAll('[data-catalog-wrapper]');
+// const images = document.querySelectorAll('[data-catalog-image]');
 
-const images = document.querySelectorAll('[data-catalog-image]');
+// const noImages = document.querySelectorAll('[data-catalog-text]');
 
-const noImages = document.querySelectorAll('[data-catalog-text]');
-
-const decors = document.querySelectorAll('[data-catalog-decor]');
+// const decors = document.querySelectorAll('[data-catalog-decor]');
 
 function removeImage() {
+  const wrappers = document.querySelectorAll('[data-catalog-wrapper]');
 
+  /* for (const wrapper of wrappers) {
+    const catalogImage = wrapper.querySelector('[data-catalog-image]');
+    if (catalogImage) {
+      wrapper.querySelector('[data-catalog-decor]').style.display = 'none';
+      wrapper.querySelector('[data-catalog-text]').style.display = 'none';
+    }
+  }*/
+
+  // вариант с forEach
+  wrappers.forEach((wrapper) => {
+    const catalogImage = wrapper.querySelector('[data-catalog-image]');
+    if (catalogImage) {
+      wrapper.querySelector('[data-catalog-decor]').style.display = 'none';
+      wrapper.querySelector('[data-catalog-text]').style.display = 'none';
+    }
+  });
+
+  /* не рабочий скрипт, скрывает только крайний item
   for (let i = 0; i < wrappers.length; i++) {
     if (!images[i]) {
       noImages[i].classList.add('catalog__image-text--add-image');
@@ -16,7 +33,7 @@ function removeImage() {
       noImages[i].classList.remove('catalog__image-text--add-image');
       decors[i].classList.remove('catalog__decor--add-image');
     }
-  }
+  }*/
 }
 
 
